7e57a297eabe97f3a6a4688d1ca297c26db870b3
huyaoguo <huyaoguo@100tal.com>
2021-03-26 18:13:51 +0800

    更改url配置

--- conan-ui/src/api/conf.js
+++ conan-ui/src/api/conf.js
@@ -1,5 +1,5 @@
 export default ({
-    url: 'http://127.0.0.1',
-    baseUrl: 'http://127.0.0.1',
-    publicUrl:'http://127.0.0.1'
+    url: 'http://127.0.0.1:8081',
+    baseUrl: 'http://127.0.0.1:8081',
+    publicUrl:'http://127.0.0.1:8081'
 })
